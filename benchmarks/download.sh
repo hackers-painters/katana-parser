b1773b68724984f15731b62adf98ada7252daddb
QFish <qfish.cn@gmail.com>
2015-04-13 03:35:11 +0800

    Added download script.

---
+++ benchmarks/download.sh
@@ -0,0 +1,8 @@
+# The benchmark css files, you can download them manually.
+curl http://hackers-painters.github.io/katana-parser/benchmarks/input.css -o input.cs
+curl http://hackers-painters.github.io/katana-parser/benchmarks/normalize.css - normalize.css
+curl http://hackers-painters.github.io/katana-parser/benchmarks/semantic.css - semantic.css
+curl http://hackers-painters.github.io/katana-parser/benchmarks/bootstrap.css - bootstrap.css
+curl http://hackers-painters.github.io/katana-parser/benchmarks/mediaquery.css - mediaquery.css
+curl http://hackers-painters.github.io/katana-parser/benchmarks/selector.css - selector.css
+curl http://hackers-painters.github.io/katana-parser/benchmarks/topcoat.css - topcoat.css
